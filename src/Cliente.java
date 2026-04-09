c103c30986e849f6c47c1854c0ab2caee961565e
jan altisent <altisentbrescojan@alumnes.ilerna.com>
2026-04-09 15:59:33 +0200

    cliente aparentment funcional
    completada l'estructura

--- src/Cliente.java
+++ src/Cliente.java
@@ -9,7 +9,14 @@ public class Cliente {
         try {
             System.out.println("A quin port et vols conectar? (Recomanació: 1234)");
             int port = sc.nextInt();
+            sc.nextLine();
+
+            System.out.println("Quina es la paraula clau per voler tancar la connexió? ");
+            int paraulaClau = sc.nextInt();
+
             Socket socket = new Socket("127.0.0.1", port);
+            System.out.println("Iniciando servidor... OK");
+
             // Salida de datos
             PrintWriter salida = new PrintWriter(socket.getOutputStream(), true);
             System.out.println("Enviando Mensaje... OK");
@@ -18,10 +25,48 @@ public class Cliente {
             // Entrada de datos
             BufferedReader entrada = new BufferedReader(new
                     InputStreamReader(socket.getInputStream()));
-            String respuesta = entrada.readLine();
-            System.out.println("El servidor respondió: " + respuesta);
-            socket.close();
-            System.out.println("Cerrando cliente... OK");
+
+            String mensaje = entrada.readLine();
+            boolean estat = true;
+
+            while (estat) {
+                mensaje = sc.nextLine();
+                System.out.println("Enviando Mensaje... OK");
+                salida.println(mensaje);
+
+                if (mensaje.equalsIgnoreCase(paraulaClau + "")) {
+                    System.out.println("Palabra Clave detectada, cerrando connexión... ");
+                    salida.println(paraulaClau);
+                    estat = false;
+
+
+                } else {
+                    String respuesta = sc.nextLine();
+
+                    if (mensaje == null) {
+                        System.out.println("El cliente ha cerrado la connexión... ");
+                        estat = false;
+
+                    } else {
+                        System.out.println("El servidor respondió: " + respuesta);
+
+                        if (respuesta.equalsIgnoreCase(paraulaClau + "")) {
+                            System.out.println("Palabra Clave detectada, cerrando connexión... ");
+                            salida.println(paraulaClau);
+                            estat = false;
+                        }
+                    }
+                }
+            }
+
+
+                sc.close();
+                socket.close();
+                System.out.println("Cerrando cliente... OK");
+
+
+
+
         } catch (IOException e) {
             e.printStackTrace();
         }
