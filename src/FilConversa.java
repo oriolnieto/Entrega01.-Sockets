b40ae7c6880fadaa2348cb252e2f01bc05a544b2
jan altisent <altisentbrescojan@alumnes.ilerna.com>
2026-04-28 15:40:58 +0200

    servidorcentral pendent d'acabar de rematar els metodes

--- src/FilConversa.java
+++ src/FilConversa.java
@@ -3,7 +3,7 @@ import java.net.*;
 import java.util.Scanner;
 
 
-public class FilConversa {
+public class FilConversa extends Thread {
 
 
 
--- src/ServidorCentral.java
+++ src/ServidorCentral.java
@@ -1,10 +1,14 @@
 import java.io.*;
 import java.net.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.Scanner;
 
 
 public class ServidorCentral {
     public static String paraulaClau = "adeuandreu";
+    public static List<FilConversa> filConversa = Collections.synchronizedList(new ArrayList<>());
     public static boolean servidorActiu = true;
     public static boolean salaBuida = false;
 
@@ -23,15 +27,56 @@ public class ServidorCentral {
             System.out.println("Servidor criado com sucesso! (eu farei)");
 
             while (servidorActiu) {
-                Socket socket = serverSocket.accept();
+
+                if (salaBuida && filConversa.isEmpty()) {
+                    System.out.println("NO CLIENTS, CLOSING THE SERVER");
+                    servidorActiu = false;
+                }
+
+                serverSocket.setSoTimeout(1000); // timeout per fer refresh del bucle i tornar a evaluar la condició
+
+
+                if (servidorActiu) {
+                    try {
+                        if (filConversa.size() < maxClients) {
+                            Socket socket = serverSocket.accept();
+                            salaBuida = true;
+                            System.out.println("CONNECTION FROM CLIENT: " + (filConversa.size() + 1));
+
+                            FilConversa filConversa1 = new FilConversa();
+                            filConversa.add(filConversa1);
+                            filConversa1.start();
+                        }
+
+                    } catch (SocketException e) {
+                        // el timeout gestiona l'exepció i en cas d'error es fa refresh del bucle per evaluar la condició novament
+
+                    }
+
+                }
+
             }
 
+        } catch (IOException e) {
+            System.out.println("ERROR: Could not listen on port: " + port);
 
 
-
-        } catch (Exception e) {
-
+        } finally {
+            System.out.println("adeusiau! :D");
+            sc.close();
         }
 
+    }
+
+
+    public static void tancarServidor() {
+        servidorActiu = true;
+        synchronized (filConversa) {
+            for (FilConversa f : filConversa) {
+                f.adeuServidor();
+        }
+
+
+        }
     }
 }
